d115e5bd15b657e8ebba5d06ae818e5a426f31af
flafi99 <flavia.pacheco.freitas@escola.pr.gov.br>
2025-10-29 10:15:39 -0300

    c

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
+
+avanca.forEach1(button => {
+button.addEventListener('click',function (){
+const atual = document.querySelector('.ativo')
+const prixomoPasso = 'passo-' + getAttribute('data-proximo')
+
+atual.classList.remove('ativo')
+document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
